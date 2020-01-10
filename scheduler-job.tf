5c6144b49a57b51f200f3cd36d24ec4d74c341b6
Boite Witte <info@boite.nl>
2020-01-10 16:31:56 +0100

    Initial terraform configuration

---
+++ scheduler-job.tf
@@ -0,0 +1,97 @@
+
+resource "google_service_account" "this" {
+  count = var.http_oauth_token != null ? (var.http_oauth_token.create ? 1 : 0) : 0
+
+  account_id   = var.http_oauth_token.service_account
+  display_name = "${var.name} - ${var.http_oauth_token.service_account}"
+  description  = "CloudScheduler Job: ${var.name}"
+
+  project = var.project_id
+}
+
+locals {
+  oauth_service_account_email = (
+    var.http_oauth_token != null
+    ? (
+      length(google_service_account.this) > 0 ? google_service_account.this[0].email : var.http_oauth_token.service_account
+    )
+    : ""
+  )
+
+  pubsub_target = (
+    var.pubsub != null
+    ? [{
+      topic_name = var.pubsub.topic_name
+      data       = var.pubsub.message_data
+      attributes = var.pubsub.message_attributes
+    }]
+    : []
+  )
+
+  http_target = (
+    var.http != null
+    ? [
+      merge(
+        {
+          uri = join(
+            "?",
+            compact(
+              concat(
+                [module.main.https_trigger_url],
+                var.http.query_string != null ? [var.http.query_string] : []
+              )
+            )
+          )
+          http_method = var.http.http_method
+          body        = var.http.body
+          headers     = var.http.headers
+          oauth_token = (
+            local.oauth_service_account_email != ""
+            ? [{
+              service_account_email = local.oauth_service_account_email
+              scope                 = var.http_oauth_token.scope != null ? var.http_oauth_token.scope : "https://www.googleapis.com/auth/cloud-platform"
+            }]
+            : []
+          )
+        }
+      )
+    ]
+    : []
+  )
+}
+
+resource "google_cloud_scheduler_job" "this" {
+  name        = var.name
+  description = var.description
+  schedule    = var.schedule
+
+  dynamic "http_target" {
+    for_each = local.http_target
+
+    content {
+      uri         = http_target.value.uri
+      http_method = http_target.value.http_method
+      body        = http_target.value.body
+      headers     = http_target.value.headers
+
+      dynamic "oauth_token" {
+        foreach = http_target.value.oauth_token
+
+        content {
+          service_account_email = oauth_token.value.service_account_email
+          scope                 = oauth_token.value.scope
+        }
+      }
+    }
+  }
+
+  dynamic "pubsub_target" {
+    for_each = local.pubsub_target
+
+    content {
+      topic_name = pubsub_target.value.topic_name
+      data       = pubsub_target.value.data
+      attributes = pubsub_target.value.attributes
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,152 @@
+variable "available_memory_mb" {
+  type        = string
+  description = "Memory (in MB), available to the function. Default value is 256MB. Allowed values are: 128MB, 256MB, 512MB, 1024MB, and 2048MB."
+  default     = "256MB"
+}
+
+variable "description" {
+  type        = string
+  description = "Description of the Cloudfunction"
+  default     = null
+}
+
+variable "environment_variables" {
+  type        = map(string)
+  description = "A set of key/value environment variable pairs to assign to the function."
+  default     = null
+}
+
+variable "entry_point" {
+  type        = string
+  description = "Name of the function that will be executed when the Google Cloud Function is triggered."
+}
+
+variable "event_triggers" {
+  type = list(object({
+    event_type = string
+    resource   = string
+    retry      = bool
+  }))
+  description = "A source that fires events in response to a condition in another service. Can't be used in conjunction with trigger_http"
+  default     = []
+}
+
+variable "http" {
+  type = object({
+    http_method  = string
+    body         = string
+    query_string = string
+    headers      = map(string)
+  })
+  description = "Configuration for the HTTP trigger"
+  default     = null
+}
+
+variable "http_oauth_token" {
+  type = object({
+    create          = bool
+    service_account = string
+    scope           = string
+  })
+  description = "Service acccount for Triggering the Function"
+  default     = null
+}
+
+variable "function_source" {
+  type = object({
+    name      = string
+    bucket    = string
+    directory = string
+    file      = string
+  })
+  description = "Defines the Source configuration for the Cloudfunction. Can't be used in conjunction with source_repository_url"
+  default     = null
+}
+
+variable "invokers" {
+  type        = list(string)
+  description = "List of IAM members who are allowed to invoke the function"
+  default     = []
+}
+
+variable "is_public_function" {
+  type        = bool
+  description = "Gives all users the right to invoke the function"
+  default     = false
+}
+
+variable "labels" {
+  type        = map(string)
+  description = "A set of key/value label pairs to assign to the function."
+  default     = {}
+}
+
+variable "max_instances" {
+  type        = number
+  description = "The limit on the maximum number of function instances that may coexist at a given time."
+  default     = null
+}
+
+variable "name" {
+  type        = string
+  description = "A user-defined name of the function. Function names must be unique globally."
+}
+
+variable "project_id" {
+  type        = string
+  description = "The Project ID to which the function will be deployed"
+  default     = null
+}
+
+variable "project_roles" {
+  type        = list(object({ project_id = string, role = string }))
+  description = "List of roles within other projects that will be attached to the Cloudfunction"
+  default     = []
+}
+
+variable "region" {
+  type        = string
+  description = "Region to which the Cloudfunction will be deployed"
+}
+
+variable "runtime" {
+  type        = string
+  description = "The runtime in which the function is going to run."
+}
+
+variable "roles" {
+  type        = list(string)
+  description = "List of roles that will be attached to the Cloudfunction within the same project"
+  default     = []
+}
+
+variable "source_repository_url" {
+  type        = string
+  description = "The URL pointing to the hosted Cloud Source repository where the function is defined."
+  default     = null
+}
+
+variable "timeout" {
+  type        = number
+  description = "Timeout (in seconds) for the function. Default value is 60 seconds. Cannot be more than 540 seconds."
+  default     = 60
+}
+
+variable "pubsub" {
+  type = object({
+    topic_name                  = string
+    create                      = bool
+    message_data                = string
+    message_attributes          = map(string)
+    kms_key_name                = string
+    allowed_persistence_regions = list(string)
+  })
+  description = "The configuration for the PubSub, if null, the HTML trigger will be used"
+  default     = null
+}
+
+variable "vpc_connector" {
+  type        = string
+  description = "The VPC Network Connector that this cloud function can connect to."
+  default     = null
+}
